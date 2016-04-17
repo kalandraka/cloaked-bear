7c528df8fca5107d1917719ef461d601e6865b3f
Richarddr <richarddr@openmailbox.org>
2016-04-17 18:52:22 -0400

    [Update]Adicionado UnitTest ConfiguracionMarchamoController.

--- src/Buseta/CombustibleBundle/Tests/Controller/ConfiguracionMarchamoControllerTest.php
+++ src/Buseta/CombustibleBundle/Tests/Controller/ConfiguracionMarchamoControllerTest.php
@@ -4,55 +4,17 @@ namespace Buseta\BodegaBundle\Tests\Controller;
 
 use Buseta\BusesBundle\Tests\Controller\AbstractWebTestCase;
 
-class ConfiguracionCombustibleControllerTest extends AbstractWebTestCase
+class ConfiguracionMarchamoControllerTest extends AbstractWebTestCase
 {
     public function testIndex()
     {
-        // Test search/list ConfiguracionCombustible
-        $crawler = $this->client->request('GET', '/configuracion/combustible/');
-        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /configuracion/combustible/");
+        // Test search/list ConfiguracionMarchamo
+        $crawler = $this->client->request('GET', '/configuracion/marchamo/');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /configuracion/marchamo/");
 
         $this->assertGreaterThan(0, $crawler->filter('a[href$=\'new\']')->count(), 'Missing add new button');
     }
 
-    public function testShow()
-    {
-        // Test search/list ConfiguracionCombustible
-        $crawler = $this->client->request('GET', '/configuracion/combustible/');
-        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /configuracion/combustible/");
-
-        // Check data in the index view
-        $this->assertGreaterThan(0, $crawler->filter('a[href$=\'show\']')->count(), 'Missing elements/test cases');
-        $crawler = $this->client->click($crawler->filter('a[href$=\'show\']')->eq(0)->link());
-        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /configuracion/combustible/\\d+/show");
-
-        // Check buttons in show view
-        //$this->checkFunctionalityButtons($crawler, array('Editar', 'Volver', 'Eliminar'), 'Edit');
-    }
-
-    public function testEdit()
-    {
-        // Test search/list ConfiguracionCombustible
-        $crawler = $this->client->request('GET', '/configuracion/combustible/');
-        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /configuracion/combustible/");
-
-        // Check data in the index view
-        $this->assertGreaterThan(0, $crawler->filter('a[href$=\'edit\']')->count(), 'Missing elements/test cases');
-        $crawler = $this->client->click($crawler->filter('a[href$=\'edit\']')->eq(0)->link());
-        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /configuracion/combustible/\\d+/edit");
-
-        // Check buttons in edit view
-        //$this->checkFunctionalityButtons($crawler, array('Salvar', 'Volver'), 'Edit');
-    }
-
-    public function testNew()
-    {
-        // Test create ConfiguracionCombustible
-        $crawler = $this->client->request('GET', '/configuracion/combustible/new');
-        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /configuracion/combustible/new");
-
-        //$this->checkFunctionalityButtons($crawler, array('Salvar', 'Volver'), 'New');
-    }
 
     private function checkFunctionalityButtons(\Symfony\Component\DomCrawler\Crawler $crawler, array $links, $view)
     {
