b0466f7c2eff85b7d9d03c54717af7e9da92dc80
Richarddr <richarddr@openmailbox.org>
2016-04-30 17:57:42 -0400

    [Update]Adicionado UnitTest InformeController.

--- src/Buseta/BodegaBundle/Tests/Controller/InformeControllerTest.php
+++ src/Buseta/BodegaBundle/Tests/Controller/InformeControllerTest.php
@@ -4,27 +4,27 @@ namespace Buseta\BodegaBundle\Tests\Controller;
 
 use Buseta\BusesBundle\Tests\Controller\AbstractWebTestCase;
 
-class AlbaranControllerTest extends AbstractWebTestCase
+class InformeControllerTest extends AbstractWebTestCase
 {
     public function testIndex()
     {
-        // Test search/list Albaran
-        $crawler = $this->client->request('GET', '/albaran/');
-        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /albaran/");
+        // Test search/list Informe
+        $crawler = $this->client->request('GET', '/informe/informeMovimiento/');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /informe/informeMovimiento/");
 
         $this->assertGreaterThan(0, $crawler->filter('a[href$=\'new\']')->count(), 'Missing add new button');
     }
 
     public function testShow()
     {
-        // Test search/list bodega
-        $crawler = $this->client->request('GET', '/albaran/');
-        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /albaran/");
+        // Test search/list Informe
+        $crawler = $this->client->request('GET', '/informe/informeMovimiento/');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /informe/informeMovimiento/");
 
         // Check data in the index view
         $this->assertGreaterThan(0, $crawler->filter('a[href$=\'show\']')->count(), 'Missing elements/test cases');
         $crawler = $this->client->click($crawler->filter('a[href$=\'show\']')->eq(0)->link());
-        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /albaran/\\d+/show");
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /informe/informeMovimiento/\\d+/show");
 
         // Check buttons in show view
         //$this->checkFunctionalityButtons($crawler, array('Editar', 'Volver', 'Eliminar'), 'Edit');
@@ -32,14 +32,14 @@ class AlbaranControllerTest extends AbstractWebTestCase
 
     public function testEdit()
     {
-        // Test search/list bodega
-        $crawler = $this->client->request('GET', '/albaran/');
-        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /albaran/");
+        // Test search/list Informe
+        $crawler = $this->client->request('GET', '/informe/informeMovimiento/');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /informe/informeMovimiento/");
 
         // Check data in the index view
         $this->assertGreaterThan(0, $crawler->filter('a[href$=\'edit\']')->count(), 'Missing elements/test cases');
         $crawler = $this->client->click($crawler->filter('a[href$=\'edit\']')->eq(0)->link());
-        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /albaran/\\d+/edit");
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /informe/informeMovimiento/\\d+/edit");
 
         // Check buttons in edit view
         //$this->checkFunctionalityButtons($crawler, array('Salvar', 'Volver'), 'Edit');
@@ -47,9 +47,9 @@ class AlbaranControllerTest extends AbstractWebTestCase
 
     public function testNew()
     {
-        // Test create bodega
-        $crawler = $this->client->request('GET', '/albaran/new');
-        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /albaran/new");
+        // Test create Informe
+        $crawler = $this->client->request('GET', '/informe/informeMovimiento/new');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /informe/informeMovimiento/new");
 
         //$this->checkFunctionalityButtons($crawler, array('Salvar', 'Volver'), 'New');
     }
