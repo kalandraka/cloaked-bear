592f826785ad75b6eea7ddf834560378a12a183f
Richarddr <richarddr@openmailbox.org>
2016-05-12 15:39:47 -0400

    [Hotfix] Modificado InformeControllerTest.

--- src/Buseta/BodegaBundle/Tests/Controller/InformeControllerTest.php
+++ src/Buseta/BodegaBundle/Tests/Controller/InformeControllerTest.php
@@ -9,49 +9,9 @@ class InformeControllerTest extends AbstractWebTestCase
     public function testIndex()
     {
         // Test search/list Informe
-        $crawler = $this->client->request('GET', '/informe/informeMovimiento/');
-        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /informe/informeMovimiento/");
+        $crawler = $this->client->request('GET', '/informe/informeMovimiento');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /informe/informeMovimiento");
 
-        $this->assertGreaterThan(0, $crawler->filter('a[href$=\'new\']')->count(), 'Missing add new button');
-    }
-
-    public function testShow()
-    {
-        // Test search/list Informe
-        $crawler = $this->client->request('GET', '/informe/informeMovimiento/');
-        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /informe/informeMovimiento/");
-
-        // Check data in the index view
-        $this->assertGreaterThan(0, $crawler->filter('a[href$=\'show\']')->count(), 'Missing elements/test cases');
-        $crawler = $this->client->click($crawler->filter('a[href$=\'show\']')->eq(0)->link());
-        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /informe/informeMovimiento/\\d+/show");
-
-        // Check buttons in show view
-        //$this->checkFunctionalityButtons($crawler, array('Editar', 'Volver', 'Eliminar'), 'Edit');
-    }
-
-    public function testEdit()
-    {
-        // Test search/list Informe
-        $crawler = $this->client->request('GET', '/informe/informeMovimiento/');
-        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /informe/informeMovimiento/");
-
-        // Check data in the index view
-        $this->assertGreaterThan(0, $crawler->filter('a[href$=\'edit\']')->count(), 'Missing elements/test cases');
-        $crawler = $this->client->click($crawler->filter('a[href$=\'edit\']')->eq(0)->link());
-        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /informe/informeMovimiento/\\d+/edit");
-
-        // Check buttons in edit view
-        //$this->checkFunctionalityButtons($crawler, array('Salvar', 'Volver'), 'Edit');
-    }
-
-    public function testNew()
-    {
-        // Test create Informe
-        $crawler = $this->client->request('GET', '/informe/informeMovimiento/new');
-        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /informe/informeMovimiento/new");
-
-        //$this->checkFunctionalityButtons($crawler, array('Salvar', 'Volver'), 'New');
     }
 
     private function checkFunctionalityButtons(\Symfony\Component\DomCrawler\Crawler $crawler, array $links, $view)
