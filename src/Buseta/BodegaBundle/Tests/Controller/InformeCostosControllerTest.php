9a1cc90c1914bd7f202beca882184dec85b4db22
Richarddr <richarddr@openmailbox.org>
2016-05-12 15:40:34 -0400

    [Hotfix] Modificado InformeCostosControllerTest.

--- src/Buseta/BodegaBundle/Tests/Controller/InformeCostosControllerTest.php
+++ src/Buseta/BodegaBundle/Tests/Controller/InformeCostosControllerTest.php
@@ -12,46 +12,6 @@ class InformeCostosControllerTest extends AbstractWebTestCase
         $crawler = $this->client->request('GET', '/informecostos/');
         $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /informecostos/");
 
-        $this->assertGreaterThan(0, $crawler->filter('a[href$=\'new\']')->count(), 'Missing add new button');
-    }
-
-    public function testShow()
-    {
-        // Test search/list informecostos
-        $crawler = $this->client->request('GET', '/informecostos/');
-        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /informecostos/");
-
-        // Check data in the index view
-        $this->assertGreaterThan(0, $crawler->filter('a[href$=\'show\']')->count(), 'Missing elements/test cases');
-        $crawler = $this->client->click($crawler->filter('a[href$=\'show\']')->eq(0)->link());
-        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /informecostos/\\d+/show");
-
-        // Check buttons in show view
-        //$this->checkFunctionalityButtons($crawler, array('Editar', 'Volver', 'Eliminar'), 'Edit');
-    }
-
-    public function testEdit()
-    {
-        // Test search/list informecostos
-        $crawler = $this->client->request('GET', '/informecostos/');
-        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /informecostos/");
-
-        // Check data in the index view
-        $this->assertGreaterThan(0, $crawler->filter('a[href$=\'edit\']')->count(), 'Missing elements/test cases');
-        $crawler = $this->client->click($crawler->filter('a[href$=\'edit\']')->eq(0)->link());
-        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /informecostos/\\d+/edit");
-
-        // Check buttons in edit view
-        //$this->checkFunctionalityButtons($crawler, array('Salvar', 'Volver'), 'Edit');
-    }
-
-    public function testNew()
-    {
-        // Test create informecostos
-        $crawler = $this->client->request('GET', '/informecostos/new');
-        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /informecostos/new");
-
-        //$this->checkFunctionalityButtons($crawler, array('Salvar', 'Volver'), 'New');
     }
 
     private function checkFunctionalityButtons(\Symfony\Component\DomCrawler\Crawler $crawler, array $links, $view)
