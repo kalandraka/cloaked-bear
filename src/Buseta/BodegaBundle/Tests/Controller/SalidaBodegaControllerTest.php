dbfa52f7175438493e365a6c9d116a6bf1fdae1c
Richarddr <richarddr@openmailbox.org>
2016-05-12 15:44:00 -0400

    [Hotfix]Modificado SalidaBodegaControllerTest.

--- src/Buseta/BodegaBundle/Tests/Controller/SalidaBodegaControllerTest.php
+++ src/Buseta/BodegaBundle/Tests/Controller/SalidaBodegaControllerTest.php
@@ -9,7 +9,7 @@ class SalidaBodegaControllerTest extends AbstractWebTestCase
     public function testIndex()
     {
         // Test search/list SalidaBodega
-        $crawler = $this->client->request('GET', '/bodega/salidabodega/salidabodega/');
+        $crawler = $this->client->request('GET', '/bodega/salidabodega/salidabodega');
         $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /bodega/salidabodega/salidabodega/");
 
         $this->assertGreaterThan(0, $crawler->filter('a[href$=\'new\']')->count(), 'Missing add new button');
@@ -18,13 +18,13 @@ class SalidaBodegaControllerTest extends AbstractWebTestCase
     public function testShow()
     {
         // Test search/list SalidaBodega
-        $crawler = $this->client->request('GET', '/bodega/salidabodega/');
-        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /bodega/salidabodega/");
+        $crawler = $this->client->request('GET', '/bodega/salidabodega/salidabodega');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /bodega/salidabodega/salidabodega");
 
         // Check data in the index view
         $this->assertGreaterThan(0, $crawler->filter('a[href$=\'show\']')->count(), 'Missing elements/test cases');
         $crawler = $this->client->click($crawler->filter('a[href$=\'show\']')->eq(0)->link());
-        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /bodega/salidabodega/\\d+/show");
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /bodega/salidabodega/salidabodega\\d+/show");
 
         // Check buttons in show view
         //$this->checkFunctionalityButtons($crawler, array('Editar', 'Volver', 'Eliminar'), 'Edit');
@@ -33,13 +33,13 @@ class SalidaBodegaControllerTest extends AbstractWebTestCase
     public function testEdit()
     {
         // Test search/list SalidaBodega
-        $crawler = $this->client->request('GET', '/bodega/salidabodega/');
-        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /bodega/salidabodega/");
+        $crawler = $this->client->request('GET', '/bodega/salidabodega/salidabodega');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /bodega/salidabodega/salidabodega");
 
         // Check data in the index view
         $this->assertGreaterThan(0, $crawler->filter('a[href$=\'edit\']')->count(), 'Missing elements/test cases');
         $crawler = $this->client->click($crawler->filter('a[href$=\'edit\']')->eq(0)->link());
-        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /bodega/salidabodega/\\d+/edit");
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /bodega/salidabodega/salidabodega\\d+/edit");
 
         // Check buttons in edit view
         //$this->checkFunctionalityButtons($crawler, array('Salvar', 'Volver'), 'Edit');
