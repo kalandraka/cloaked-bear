1413d7d029aa7d2a4e2362d02ba551249ca9bf5a
Dundivet <dundivet@gmail.com>
2016-05-31 07:12:26 -0400

    [Update] Refactorizando versión 20160530164231 Doctrine Migrations.

--- app/DoctrineMigrations/Version20160530164231.php
+++ app/DoctrineMigrations/Version20160530164231.php
@@ -21,21 +21,20 @@ class Version20160530164231 extends AbstractMigration
         $this->addSql('DROP TABLE conf_cambio_hora');
         $this->addSql('DROP TABLE conf_fecha_sistema');
         $this->addSql('ALTER TABLE d_bitacora_almacen ADD qty INT DEFAULT NULL');
+        $this->addSql('UPDATE d_bitacora_almacen b SET b.qty = b.movement_qty WHERE b.movement_type IN (\'V+\', \'M+\', \'P+\', \'I+\', \'D+\')');
+        $this->addSql('UPDATE d_bitacora_almacen b SET b.qty = - b.movement_qty WHERE b.movement_type IN (\'V-\', \'M-\', \'P-\', \'I-\', \'D-\')');
+        $this->addSql('ALTER TABLE d_bitacora_serial ADD qty INT DEFAULT NULL');
+        $this->addSql('UPDATE d_bitacora_serial bs SET bs.qty = 1 WHERE bs.movement_type IN (\'V+\', \'M+\', \'P+\', \'I+\', \'D+\')');
+        $this->addSql('UPDATE d_bitacora_serial bs SET bs.qty = -1 WHERE bs.movement_type IN (\'V-\', \'M-\', \'P-\', \'I-\', \'D-\')');
         $this->addSql('CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_GetSerialesProductoEnAlmacenObjectsPlus`(IN `warehouse_id` INT, IN `product_id` INT)
     NO SQL
 BEGIN
-
-declare montoPositivo integer;
-
-declare montoNegativo integer;
-
 DROP TEMPORARY TABLE IF EXISTS  bitacoras_serial_producto_almacen;
 CREATE TEMPORARY TABLE IF NOT EXISTS bitacoras_serial_producto_almacen
 SELECT *  from d_bitacora_serial b
 WHERE b.product_id = product_id and b.warehouse_id= warehouse_id ;
 
-
- select *, sum(cantidad) as cant
+select *, sum(qty) as cant
 from bitacoras_serial_producto_almacen
 group by serial
 having cant>0;
@@ -44,25 +43,18 @@ END');
         $this->addSql('CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_GetSerialesProductoEnAlmacenPlus`(IN `warehouse_id` INT, IN `product_id` INT)
     NO SQL
 BEGIN
-
-declare montoPositivo integer;
-
-declare montoNegativo integer;
-
-
 DROP TEMPORARY TABLE IF EXISTS  bitacoras_serial_producto_almacen;
 CREATE TEMPORARY TABLE IF NOT EXISTS bitacoras_serial_producto_almacen
-SELECT cantidad , serial  from d_bitacora_serial b
+SELECT qty , serial  from d_bitacora_serial b
 WHERE b.product_id = product_id and b.warehouse_id= warehouse_id ;
 
 select res.serial from
 (
- select serial, sum(cantidad) as cant
+ select serial, sum(qty) as cant
 from bitacoras_serial_producto_almacen
 group by serial
 having cant>0
  ) res ;
-
 
 END');
     }
@@ -78,6 +70,7 @@ END');
         $this->addSql('CREATE TABLE conf_cambio_hora (id INT AUTO_INCREMENT NOT NULL, hora TIME DEFAULT NULL, activo TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('CREATE TABLE conf_fecha_sistema (id INT AUTO_INCREMENT NOT NULL, fecha DATE DEFAULT NULL, activo TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE d_bitacora_almacen DROP qty');
+        $this->addSql('ALTER TABLE d_bitacora_serial DROP qty');
         $this->addSql('DROP PROCEDURE `sp_GetSerialesProductoEnAlmacenObjectsPlus`');
         $this->addSql('DROP PROCEDURE `sp_GetSerialesProductoEnAlmacenPlus`');
     }
